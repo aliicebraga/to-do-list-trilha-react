21dfab1e9cc918cb4becd3c695fdc6c80a13cfe4
aliicebraga <aliicebraga.dev@gmail.com>
2022-03-30 11:15:07 -0300

    Challenge Complete :nerd_face:

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,49 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    // checar se o campo do input não está vazio
+    // se possuir um valor, executa o escopo
+    if(newTaskTitle){
+      //nova task do tipo Task com o id random, title é o valor do input e isComplete inicia como false
+      const newTask: Task = {
+        id: Math.random(),
+        title: newTaskTitle,
+        isComplete:false
+      }
+      //utilizando o set do estado para adicionar a nova task
+      //com o callback "oldState" garante que os valores antigos estejam atualizados, pois a renderização é assíncrona
+      //o spread operator ...oldState pega os valores antigos e salva junto com a nova task
+      setTasks(oldState => [...oldState, newTask]);
+
+      //setar o input controlado para o estado inicial, limpa o input
+      setNewTaskTitle('');
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+    // para marcar a task como completa é necessário mapear a lista de tasks e encontrar a task com o id igual ao selecionado
+    // na task procurada é preciso alterar o atributo isComplete, é possível fazer isso negando seu estado atual,
+    // então se o isComplete for false, muda pra true. e vice-versa
+    //para as demais tasks nada muda
+    const newTasks = tasks.map(task => task.id == id ? { ...task, isComplete : !task.isComplete} : task);
+
+    //após isso seta novamente a lista de tasks
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    //criar um novo estado ao invés de modificar um existente
+    //então, segue-se o conceito de criar uma nova lista de tasks com todos que tenham o id diferente do selecionado
+    //para isso usa-se um filtro
+    const newTasks = tasks.filter(task => task.id != id);
+
+    //após isso seta novamente a lista de tasks
+    setTasks(newTasks);
   }
 
   return (
